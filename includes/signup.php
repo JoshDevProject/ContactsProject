d3a2e8be0111863d1a122dc802e153e0f6e26413
Admin <Admin@172.29.20.38>
2012-06-28 10:29:15 -0700

    fixed perms table insert query

--- includes/signup.php
+++ includes/signup.php
@@ -2,21 +2,18 @@
 
 include 'database_functions.php';
 
-//start the session
-session_start();
-
 //retrieve user information from signup.html
-$firstname = $_POST['firstname'];
-$lastname = $_POST['lastname'];
-$email = $_POST ['email'];
-$username = $_POST['username'];
-$password = $_POST['password'];
+$firstname =    $_POST['firstname'];
+$lastname =     $_POST['lastname'];
+$email =        $_POST['email'];
+$username =     $_POST['username'];
+$password =     $_POST['password'];
 
 //add the user to the user table
 $result = query_database("INSERT into " . DBConfig::$userTable . " (firstname,lastname,email,username,password) VALUES ('$firstname','$lastname','$email','$username','$password')");
 
 //retrieve the assigned login_id for the newly created user
-$result = query_database("SELECT * FROM " . DBConfig::$userTable . " where firstname='$username'");
+$result = query_database("SELECT * FROM " . DBConfig::$userTable . " where username='$username'");
 
 //store the result in an assoc array
 $info = mysqli_fetch_assoc($result);
